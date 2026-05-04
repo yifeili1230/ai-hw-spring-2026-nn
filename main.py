ddee6b4aa27fbc7b1fab77815feb920757bef8bd
Yifei Li <45143833+yifeili1230@users.noreply.github.com>
2026-05-03 20:15:07 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+
+import torch
+from torchvision import datasets, transforms
+
+###
+transform = transforms.Compose([
+    transforms.RandomRotation(15),     
+    transforms.RandomAffine(0, shear=10, scale=(0.8, 1.2)),
+    transforms.ToTensor(),             
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.MNIST(root='./data', train=False, transform=transforms.Compose([
+    transforms.ToTensor(), 
+    transforms.Normalize((0.1307,), (0.3081,))
+]))
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1000, shuffle=False)
+
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(1600, 128) 
+        self.fc2 = nn.Linear(128, 10)   
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 1600) 
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+model = Net().to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+criterion = nn.CrossEntropyLoss()
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+
+        data, target = data.to(device), target.to(device)
+
+        optimizer.zero_grad()    
+        output = model(data)     
+        loss = criterion(output, target) 
+        loss.backward()          
+        optimizer.step()         
+        if batch_idx % 100 == 0:
+            print(f'Train Epoch: {epoch} Loss: {loss.item():.6f}')
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += criterion(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    accuracy = 100. * correct / len(test_loader.dataset)
+    print(f'\nTest set: Accuracy: {correct}/{len(test_loader.dataset)} ({accuracy:.2f}%)\n')
+
+
+
+
+num_epochs = 10
+
+for epoch in range(1, num_epochs + 1):
+    train(epoch)  
+    test()
